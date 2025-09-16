62da56680eed55cdbb8c596b14e04eb88223911b
Harikrishnan <54831389+harikriz22@users.noreply.github.com>
2025-09-16 21:56:59 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,144 @@
+# conditional statement 
+
+# outcome (bool)
+
+# True / False
+
+# if 
+# if else:
+# neste if and elif stetemet
+
+# if condition :
+    # code to be executed 
+
+# age = 12
+# # if 
+# if age > 18 :
+#     print('eligible')
+# # if else
+
+# a = 10
+# if a == 5 :
+#     print('a is five')
+# else:
+#     print('a is not five')
+
+# check if a number is even or odd 
+#largest of 2 numbers
+
+
+
+# ctrl + z
+
+
+# num = 50004
+
+# if num % 2 == 0:
+#     print('even')
+# else:
+#     print('odd')
+
+# elif 
+
+# num = 0
+# if num > 0 :
+#     print('positive')
+# elif num < 0:
+#     print('negative')
+# else:
+#     print('zero')
+
+# if a > 30:
+#     print('larger than 30')
+# if a > 20:
+#     print('larger than 20')
+# if a > 17:
+#     print('larger than 17')
+# else:
+#     print('a is very small')
+
+
+
+# if a > b :
+#     print(a,'is largest')
+# else:
+#     print(b,'is largest')
+
+# ctrl + z
+
+
+# and
+# or 
+# not
+
+
+# 2 condition 
+
+# and 
+
+
+# True True -->  True
+# True False --> False
+# False True --> False
+# False False --> False
+
+
+
+# a = 5 
+# b = 7
+
+# if a > 1 and b > a:
+#     print('yes')
+# else:
+#     print('no')
+
+
+# or
+
+# True True -->  True
+# True False --> True
+# False True --> True
+# False False --> False
+
+# a  = 7
+# if a == 1 or a > 5 or a< 4 or a == 1:
+#      print('yes')
+# # else:
+#     print('no')
+
+# not 
+
+# a = 5 
+
+# print(not a == 5)
+
+# num = 21
+
+# if not num % 5 == 0 :
+#     print(num)
+
+# largest of 2 numbers 
+
+# largest of 3 numbers 
+
+
+# a = 17
+# b = 3
+# c = 11
+
+
+# if a > b and a > c:
+#     print(a)
+# elif b > a and b > c:
+#     print(b)
+# else:
+#     print(c)
+
+
+# student_mark = 57
+
+
+# >40 --> fail 
+# 90-100 -- > A+
+# 80-90 A
+# 70-80 b+
